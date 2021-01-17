2adeddaf917626b7561d0b5b600cbdad693bc08f
wladekpal <wladek.palucki@gmail.com>
2021-01-17 12:48:43 +0100

    Memory class added

--- Memory.h
+++ Memory.h
@@ -5,6 +5,32 @@
 #ifndef OOASM_MEMORY_H
 #define OOASM_MEMORY_H
 
-/* TODO: The concept of Computer memory should be defined */
+#include <array>
+#include <map>
+#include <cstdint>
+#include <stdexcept>
+#include "Id.h"
+#include "Flags.h"
+
+template<uint64_t Size>
+class Memory {
+    private:
+        std::array<int64_t, Size> memCells;
+        std::map<Id, uint64_t> variables;
+        uint64_t numberOfVariables = 0;
+
+    public:
+        // Maybe flags should be just bool not separate classes.
+        ZF zeroFlag;
+        SF signFlag;
+
+        int64_t getMemValue(uint64_t index) const;
+
+        void changeMemValue(uint64_t index, int64_t value);
+
+        uint64_t getVarAddr(Id &name);
+
+        void declareNewVariable(Id &name, int64_t value);
+}
 
 #endif //OOASM_MEMORY_H
